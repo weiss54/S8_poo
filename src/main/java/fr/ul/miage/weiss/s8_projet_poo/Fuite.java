2254baba70e94e0ea13cc2e1d9d99776fe2a0b87
luksfit <luksfit.dev@gmail.com>
2024-05-17 10:28:03 +0200

    [ADD] Fuite

---
+++ src/main/java/fr/ul/miage/weiss/s8_projet_poo/Fuite.java
@@ -0,0 +1,58 @@
+package fr.ul.miage.weiss.s8_projet_poo;
+
+/**
+ * Classe Fuite, qui represente une fuite d'eau.
+ */
+public class Fuite {
+
+    /**
+     * Debit de la fuite.
+     */
+    private int debit;
+
+    /**
+     * Indique si la fuite est réparée.
+     */
+    private boolean reparer;
+
+    /**
+     * Constructeur par défaut.
+     */
+    public Fuite() {
+        this.debit = 0;
+        this.reparer = false;
+    }
+
+    /**
+     * Constructeur avec un parametre.
+     * @param debit
+     */
+    public Fuite(int debit) {
+        this.debit = debit;
+        this.reparer = false;
+    }
+
+    /**
+     * Permet d'indiquer que la fuite est réparée.
+     */
+    public void reparer() {
+        this.reparer = true;
+    }
+
+    /**
+     * Permet de récupérer le débit de la fuite.
+     * @return débit de la fuite.
+     */
+    public int getDebit() {
+        return debit;
+    }
+
+    /**
+     * Permet de savoir si la fuite est réparée.
+     * @return true si la fuite est réparée, false sinon.
+     */
+    public boolean isReparer() {
+        return reparer;
+    }
+
+}
